128da69b94c9f4338cc5bb1c4244a382cc7bd515
Baishali Chatterjee <142675179+Baishalichatterjee@users.noreply.github.com>
2024-08-29 12:53:49 +0530

    Add files via upload

---
+++ QRCODE/takepaymentonline.py
@@ -0,0 +1,26 @@
+import qrcode
+
+# Taking UPI ID as input
+upi_id = input("Enter your UPI ID: ")
+
+# Defining the payment URLs based on the UPI ID
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+# Create QR codes for each payment app
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+# Save the QR code to image files
+phonepe_qr.save('phonepe_qr.png')  # Corrected filename
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+# Display the QR codes (ensure Pillow library is installed)
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
+ 
+#command for running the code in terminal is python filename.py
